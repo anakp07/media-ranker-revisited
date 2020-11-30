9314a6106ee95b7e74c2ac30c5cfce9a5b6a7086
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-29 18:49:36 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
 
   #Omniauth Github callback route
   get "/auth/:provider/callback", to: "users#create", as: "omniauth_callback"
+  delete "/logout", to: "users#destroy", as: "logout"
 
   # get "/login", to: "users#login_form", as: "login"
   # post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
+  # post "/logout", to: "users#logout", as: "logout"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
